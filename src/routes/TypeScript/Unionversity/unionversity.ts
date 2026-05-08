527a73797de0fb63aebb7203cc95c3f627be89f5
SondreHaugom <sondrehaugom@gmail.com>
2026-05-08 10:47:14 +0200

    Ferdig md projketet

--- src/routes/TypeScript/Unionversity/unionversity.ts
+++ src/routes/TypeScript/Unionversity/unionversity.ts
@@ -22,18 +22,32 @@ type SearchEventsOptions = {
     eventType: 'courses' | 'groups';
 }
 
+let enrolledEvents: (Course | StudyGroup)[] = [
+
+];
+
 function searchEvents(options: SearchEventsOptions) {
     const events: (Course | StudyGroup)[] = options.eventType === 'courses' ? courses : studyGroups;
-    events.filter((event: Course | StudyGroup) => {})
 
-    
+
     return events.filter((event: Course | StudyGroup) => {
-        if (typeof options.query === 'number') {
-            return event.id === options.query;
-        }
-        if (typeof options.query === 'string') {
-            return event.keywords.includes(options.query);
-        }
-    })
+      if (typeof options.query === 'number') {
+        return event.id === options.query;
+      }
 
-}   
+      if (typeof options.query === 'string') {
+        return event.keywords.includes(options.query);
+      }
+    })
+}    
+
+function enroll(event: Course | StudyGroup) {
+  enrolledEvents = [...enrolledEvents, event];
+}
+
+const searchResults = searchEvents({query: 'art', eventType: 'courses'});
+enroll(searchResults[0]);
+console.log(enrolledEvents)
+
+
+
